720df2fe1b0cc2cee8dcd3cb6baef0daf046b87a
gabrielmj23 <gabrielmj2312@gmail.com>
2023-12-07 06:54:32 -0400

    Cambios main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
+// PENDIENTE: Elaborar utilizando threads con posix (pthreads)
 #include <iostream>
+#include <string>
+#include <thread>
+#include <SistemaVehiculo.h>
 using namespace std;
-
-int main()
-{
-    cout << "Hola mundo\n";
-    return 0;
+int main() {
+	// Declarar sistemas
+	SistemaVehiculo motor("Sistema de Motor", {});
+	// Declarar resto de sistemas…
+	// Abrir hilos de sistemas
+	thread motorThread(&SistemaVehiculo::iniciarSistema, &motor);
+	// Abrir hilos del resto…
+	// Hacer trabajo…
+	// Cerrar
 }
