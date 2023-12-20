a94753673d43917874b133ee1c8faa1bf0328f42
karimalisahili <kasahili.20@est.ucab.edu.ve>
2023-12-20 18:32:29 -0500

    menu

--- src/build-centro_garantia_vehiculos-Desktop_Qt_6_6_1_GCC_64bit-Debug/centro_garantia_vehiculos_autogen/include/ui_mainwindow.h
+++ src/build-centro_garantia_vehiculos-Desktop_Qt_6_6_1_GCC_64bit-Debug/centro_garantia_vehiculos_autogen/include/ui_mainwindow.h
@@ -10,8 +10,10 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
+#include <QtGui/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
@@ -21,25 +23,49 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
+    QAction *actionManual;
+    QAction *actionAutomatico;
+    QAction *actionAjustes;
     QWidget *centralwidget;
     QMenuBar *menubar;
+    QMenu *menuMenu;
+    QMenu *menuAleatorio;
+    QMenu *menuAjustes;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
-        MainWindow->resize(800, 600);
+        MainWindow->resize(982, 838);
+        MainWindow->setMinimumSize(QSize(982, 838));
+        MainWindow->setMaximumSize(QSize(982, 838));
+        actionManual = new QAction(MainWindow);
+        actionManual->setObjectName("actionManual");
+        actionAutomatico = new QAction(MainWindow);
+        actionAutomatico->setObjectName("actionAutomatico");
+        actionAjustes = new QAction(MainWindow);
+        actionAjustes->setObjectName("actionAjustes");
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 800, 23));
+        menubar->setGeometry(QRect(0, 0, 982, 23));
+        menuMenu = new QMenu(menubar);
+        menuMenu->setObjectName("menuMenu");
+        menuAleatorio = new QMenu(menubar);
+        menuAleatorio->setObjectName("menuAleatorio");
+        menuAjustes = new QMenu(menubar);
+        menuAjustes->setObjectName("menuAjustes");
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
         MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuMenu->menuAction());
+        menubar->addAction(menuAleatorio->menuAction());
+        menubar->addAction(menuAjustes->menuAction());
 
         retranslateUi(MainWindow);
 
@@ -48,7 +74,13 @@ public:
 
     void retranslateUi(QMainWindow *MainWindow)
     {
-        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "Centro de Garantia y Vehiculos", nullptr));
+        actionManual->setText(QCoreApplication::translate("MainWindow", "Manual", nullptr));
+        actionAutomatico->setText(QCoreApplication::translate("MainWindow", "Aleatorio", nullptr));
+        actionAjustes->setText(QCoreApplication::translate("MainWindow", "Ajustes", nullptr));
+        menuMenu->setTitle(QCoreApplication::translate("MainWindow", "Manual", nullptr));
+        menuAleatorio->setTitle(QCoreApplication::translate("MainWindow", "Aleatorio", nullptr));
+        menuAjustes->setTitle(QCoreApplication::translate("MainWindow", "Ajustes", nullptr));
     } // retranslateUi
 
 };
