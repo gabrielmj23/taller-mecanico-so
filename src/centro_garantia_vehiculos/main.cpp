82c1259f4f348ec0f27818a9954c286df710176f
karimsh2001 <karimsh2001@gmail.com>
2023-12-23 15:18:55 -0400

    tamanio de la tabla se ajusta con la cantidad de clientes

--- src/centro_garantia_vehiculos/main.cpp
+++ src/centro_garantia_vehiculos/main.cpp
@@ -17,6 +17,45 @@ public:
     QString numContacto;
 };
 
+class CustomTableWidget : public QTableWidget {
+public:
+    CustomTableWidget(QWidget* parent = nullptr) : QTableWidget(parent) {}
+
+    void populateTable(Cliente* clientes, int clientesLength) {
+        // Set the number of rows and columns in the table
+        int numRows = clientesLength;
+        int numColumns = 5;
+        setRowCount(numRows);
+        setColumnCount(numColumns);
+
+        // Set the column headers
+        QStringList headers;
+        headers << "Nombre" << "Cedula" << "Numero de Carros" << "Fecha de Registro" << "Numero de Contacto";
+        setHorizontalHeaderLabels(headers);
+
+        // Set the table to stretch to fit the available space
+        horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+        // Adjust the table height based on the number of rows
+        int tableHeight = numRows * rowHeight(0) + horizontalHeader()->height() + frameWidth() * 2;
+        setFixedHeight(tableHeight);
+
+        for (int row = 0; row < clientesLength; row++) {
+            QTableWidgetItem* itemNombre = new QTableWidgetItem(clientes[row].nombre);
+            QTableWidgetItem* itemCedula = new QTableWidgetItem(clientes[row].cedula);
+            QTableWidgetItem* itemNumCarros = new QTableWidgetItem(QString::number(clientes[row].numCarros));
+            QTableWidgetItem* itemFechaRegistro = new QTableWidgetItem(clientes[row].fechaRegistro);
+            QTableWidgetItem* itemNumContacto = new QTableWidgetItem(clientes[row].numContacto);
+
+            setItem(row, 0, itemNombre);
+            setItem(row, 1, itemCedula);
+            setItem(row, 2, itemNumCarros);
+            setItem(row, 3, itemFechaRegistro);
+            setItem(row, 4, itemNumContacto);
+        }
+    }
+};
+
 
 int main(int argc, char *argv[])
 {
@@ -39,59 +78,31 @@ int main(int argc, char *argv[])
         window.setGeometry(x, y, windowWidth, windowHeight);
     }
 
-    QTableWidget tableWidget(&window);
+    CustomTableWidget tableWidget(&window);
     tableWidget.setGeometry(10, 40, 963, 600);
-
 
     // Example array of Cliente objects
     Cliente clientes[] = {
         {"John Doe", "123456789", 2, "2021-01-01", "555-1234"},
         {"Jane Smith", "987654321", 1, "2021-02-15", "555-5678"},
+        {"Alice Johnson", "456789123", 3, "2021-03-10", "555-9012"},
         {"Alice Johnson", "456789123", 3, "2021-03-10", "555-9012"}
         // Add more objects as needed
     };
 
-    // Populate the table with data from the Cliente objects
     int clientesLength = sizeof(clientes) / sizeof(clientes[0]);
 
-    // Set the number of rows and columns in the table
-    int numRows = clientesLength;
-    int numColumns = 5;
-    tableWidget.setRowCount(numRows);
-    tableWidget.setColumnCount(numColumns);
-
-    // Set the column headers
-    QStringList headers;
-    headers << "Nombre" << "Cedula" << "Numero de Carros" << "Fecha de Registro" << "Numero de Contacto";
-    tableWidget.setHorizontalHeaderLabels(headers);
-
-    // Set the table to stretch to fit the available space
-    tableWidget.horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
-    // tableWidget.verticalHeader()->setSectionResizeMode(QHeaderView::Stretch);
-
-    for (int row = 0; row < clientesLength; row++) {
-        QTableWidgetItem* itemNombre = new QTableWidgetItem(clientes[row].nombre);
-        QTableWidgetItem* itemCedula = new QTableWidgetItem(clientes[row].cedula);
-        QTableWidgetItem* itemNumCarros = new QTableWidgetItem(QString::number(clientes[row].numCarros));
-        QTableWidgetItem* itemFechaRegistro = new QTableWidgetItem(clientes[row].fechaRegistro);
-        QTableWidgetItem* itemNumContacto = new QTableWidgetItem(clientes[row].numContacto);
-
-        tableWidget.setItem(row, 0, itemNombre);
-        tableWidget.setItem(row, 1, itemCedula);
-        tableWidget.setItem(row, 2, itemNumCarros);
-        tableWidget.setItem(row, 3, itemFechaRegistro);
-        tableWidget.setItem(row, 4, itemNumContacto);
-    }
+    tableWidget.populateTable(clientes, clientesLength);
 
     QLineEdit searchBox(&window);
     searchBox.setGeometry(10, 10, 200, 20);
-QObject::connect(&searchBox, &QLineEdit::textChanged, [&tableWidget](const QString &text){
-    for (int row = 0; row < tableWidget.rowCount(); row++) {
-        QTableWidgetItem* item = tableWidget.item(row, 0);
-        bool match = item && item->text().contains(text, Qt::CaseInsensitive);
-        tableWidget.setRowHidden(row, !match);
-    }
-});
+    QObject::connect(&searchBox, &QLineEdit::textChanged, [&tableWidget](const QString &text){
+        for (int row = 0; row < tableWidget.rowCount(); row++) {
+            QTableWidgetItem* item = tableWidget.item(row, 0);
+            bool match = item && item->text().contains(text, Qt::CaseInsensitive);
+            tableWidget.setRowHidden(row, !match);
+        }
+    });
 
     window.show();
 
